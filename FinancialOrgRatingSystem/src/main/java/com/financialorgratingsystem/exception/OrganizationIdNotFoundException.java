a03cdd4eb6934aaf4ac1e36b848ee2ef7a77e14b
cr10 <cr10@inlen8520027559.corp.capgemini.com>
2022-05-11 23:28:31 +0530

    Pushing code to Git V4

---
+++ FinancialOrgRatingSystem/src/main/java/com/financialorgratingsystem/exception/OrganizationIdNotFoundException.java
@@ -0,0 +1,11 @@
+package com.financialorgratingsystem.exception;
+
+public class OrganizationIdNotFoundException extends RuntimeException {
+
+	public OrganizationIdNotFoundException(String message) {
+
+		super(message);
+	}
+
+	
+}
